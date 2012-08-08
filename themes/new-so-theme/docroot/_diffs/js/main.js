83da72e9f547896a2c45f14a1c8ac3f4c3477784
Aaron Delani <today0@gmail.com>
2012-08-07 17:02:35 -0700

    New Javascript file to center child menus when available

---
+++ themes/new-so-theme/docroot/_diffs/js/main.js
@@ -0,0 +1,51 @@
+AUI().ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function(A) {
+		var navigation = A.one('#navigation');
+
+		navigation.all('li.has-children').each(
+			function (x){
+				var middle = (this.get('clientWidth') / 2);
+				var negMargin = "\-" + (100 - middle) + "px";
+
+				console.log(this.get('clientWidth'));
+
+				console.log(middle);
+
+				console.log(negMargin);
+
+				this.one('.child-menu').setStyle('left', negMargin);
+			}
+		);
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
