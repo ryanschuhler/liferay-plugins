66eec2cd6899801943bd1c300756da155f6350a4
ryanschuhler <ryan.schuhler@liferay.com>
2012-08-17 15:08:36 -0700

    started js click function

--- themes/new-so-theme/docroot/_diffs/js/main.js
+++ themes/new-so-theme/docroot/_diffs/js/main.js
@@ -6,16 +6,59 @@ AUI().ready(
 	*/
 
 	function(A) {
+		var body = A.one('body');
+		var mobNav = A.one('.mobile #navigation');
 		var navigation = A.one('#navigation');
+		var toggle = A.one('#toggle');
+
+		var menuTrigger = function (event) {
+			event.target.preventDefault();
+			event.currentTarget.preventDefault();
+			console.log(event.currentTarget);
+		};
 
 		navigation.all('li.has-children').each(
 			function (x){
-				var middle = Math.floor((this.get('clientWidth') / 2));
-				
+				console.log(x.one('a'));
+				var childMenu = x.one('.parent-menu');
+				var middle = Math.floor((x.get('clientWidth') / 2));
 				var negMargin = (middle - 100) + "px";
+				var anchor = x.one('a');
 
-				this.one('.child-menu').setStyle('left', negMargin);
+				anchor.delegate(
+					'click',
+					function (e) {
+						menuTrigger(e);
+					}
+				);
+
+				// childMenu.setStyle('left', negMargin);
 			}
+		);
+
+		toggle.on(
+			'click',
+			function (e) {
+				body.toggleClass('sidebar-collapsed');
+			}
+		);
+
+		mobNav.all('li.has-children').each(
+			function (node) {
+			}
+			// triangle.on(
+			// 	'click',
+			// 	function (f) {
+			// 		f.preventDefault();
+			// 		ul.toggleClass('nav-expand');
+					
+			// 		if (triangle.hasClass('nav-expand')) {
+			// 			triangle.replace('&#9656;' ,'&#9662;');
+			// 		} else {
+			// 			triangle.replace('&#9662;', '&#9656;');
+			// 		}
+			// 	}
+			// )
 		);
 	}
 );
