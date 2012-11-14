4a8a8680bee8b7f8eec1ea94f3d1199fc24a170b
ryanschuhler <ryan.schuhler@liferay.com>
2012-11-14 10:07:51 -0800

    LPS-18477 Zoe Healthcare Mobile

--- themes/zoe-healthcare-theme/docroot/_diffs/js/main.js
+++ themes/zoe-healthcare-theme/docroot/_diffs/js/main.js
@@ -1,12 +1,85 @@
 AUI().ready(
 	'aui-base',
+	'aui-io',
+	'event-move',
+	'node-base',
+	'node-event-delegate',
+	'transition',
 	/*
 	This function gets loaded when all the HTML, not including the portlets, is
 	loaded.
 	*/
 
-	function(A) {
-		A.all('#keywords').attr('title', 'Press Enter to Search');
+	function (A) {
+
+		var keywords = A.all('#keywords');
+
+		if (keywords) {
+			keywords.attr('title', 'Press Enter to Search');
+		}
+
+		var body = A.one('body');
+		var html = A.one('html');
+		var navigation = A.one('#navigation')
+		var toggle = A.one('#toggle');
+
+		if (html.hasClass('mobile')) {
+			navigation.removeClass('sort-pages modify-pages');
+
+			if(toggle) {
+				toggle.on(
+					'click',
+					function (e) {
+						body.toggleClass('show-nav');			
+					}
+				);
+			};
+
+			body.all('.desktop-link').on(
+				'click',
+				function (e) {
+					body.toggleClass('mobile-enabled')
+					
+					A.io.request(
+						themeDisplay.getPathMain() + '/portal/session_click',
+						{
+							data: {
+								'mobile-enabled': body.hasClass('mobile-enabled')
+							}
+						}
+					);
+					window.location.reload(true);		
+				}
+			);
+
+			body.on(
+				"gesturemovestart", 
+				function (e) {
+					var item = e.currentTarget;
+					var MIN_SWIPE = 20;
+					var target = e.target;
+
+					item.setData("swipeStart", e.pageX);
+
+					item.once(
+						"gesturemoveend",
+						function (e) {
+							var swipeStart = item.getData("swipeStart");
+							var swipeEnd = e.pageX;
+							var isSwipeLeft = (swipeStart-swipeEnd) > MIN_SWIPE;
+							var isSwipeRight = (swipeEnd-swipeStart) > MIN_SWIPE;
+							var navOut = body.hasClass('show-nav');
+							var swipeLeft = isSwipeLeft && navOut;
+							var swipeRight = isSwipeRight && !navOut;
+
+							if (swipeLeft || swipeRight) {
+								body.toggleClass('show-nav');
+							}
+						}
+					)					
+				}
+			)
+		}
 	}
 );
 
@@ -32,4 +105,5 @@ Liferay.on(
 
 	function() {
 	}
+>>>>>>> a9f0b11... LPS-18477 Zoe Healthcare Mobile
 );
