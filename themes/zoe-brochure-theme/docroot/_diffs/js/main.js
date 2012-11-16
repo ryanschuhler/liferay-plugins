3497acf33ab64e45f4f41cc93f0c333d95fe8326
ryanschuhler <ryan.schuhler@liferay.com>
2012-11-15 16:29:13 -0800

    LPS-17088 Zoe Brochure Mobile

---
+++ themes/zoe-brochure-theme/docroot/_diffs/js/main.js
@@ -0,0 +1,68 @@
+AUI().ready(
+	'aui-io',
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function(A) {
+		var body = A.one('body');
+		var html = A.one('html');
+		var navContent = A.one('.nav-content');
+		var navMenuButton = A.one('#nav-menu-button');
+
+		if (html.hasClass('mobile')) {
+			body.one('#desktop-link').on(
+				'click',
+				function (e) {
+					body.toggleClass('mobile-enabled')
+					
+					A.io.request(
+						themeDisplay.getPathMain() + '/portal/session_click',
+						{
+							data: {
+								'mobile-enabled': body.hasClass('mobile-enabled')
+							}
+						}
+					);
+					window.location.reload(true);		
+				}
+			);
+			
+			if (body.hasClass('mobile-enabled')) {
+				navMenuButton.on(
+					'click',
+					function () {
+						navContent.toggleClass('show-nav');
+					}
+				);
+			}
+		}
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
